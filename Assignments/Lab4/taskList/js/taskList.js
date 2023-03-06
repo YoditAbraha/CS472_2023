bb501458806ec1755c914d8ac821717610576044
YoditAbraha <107884901+YoditAbraha@users.noreply.github.com>
2023-03-05 18:41:18 -0600

    asdf

--- Assignments/Lab4/taskList/js/taskList.js
+++ Assignments/Lab4/taskList/js/taskList.js
@@ -1,17 +1,35 @@
-const addTaskButton = document.getElementById("add-task");
-    const clearTaskButton = document.getElementById("clear-task");
-    const taskListTextarea = document.getElementById("task-list");
-    const taskInput = document.getElementById("task");
 
-    addTaskButton.addEventListener("click", function() {
-      const newTask = taskInput.value;
-      if (newTask.trim() !== "") {
-        taskListTextarea.value += "- " + newTask + "\n";
-        taskInput.value = "";
-      }
-    });
+document.getElementById("tArea").innerHTML=localStorage.getItem("hello");
+    
+ 
+    function addTask(){            
+        let x=document.getElementById("tskin").value;
+       document.getElementById("tArea").value += x;
+       document.getElementById("tskin").value="";
+       localStorage.setItem("hello",document.getElementById("tArea").value );
+       
+       
+    }
+    function clearTask(){
+       
+       document.getElementById("tArea").value="";
+       
+    }
 
-    clearTaskButton.addEventListener("click", function() {
-      taskListTextarea.value = "";
-      taskInput.value = "";
-    });
+// const addTaskButton = document.getElementById("add-task");
+//     const clearTaskButton = document.getElementById("clear-task");
+//     const taskListTextarea = document.getElementById("task-list");
+//     const taskInput = document.getElementById("task");
+
+//     addTaskButton.addEventListener("click", function() {
+//       const newTask = taskInput.value;
+//       if (newTask.trim() !== "") {
+//         taskListTextarea.value += "- " + newTask + "\n";
+//         taskInput.value = "";
+//       }
+//     });
+
+//     clearTaskButton.addEventListener("click", function() {
+//       taskListTextarea.value = "";
+//       taskInput.value = "";
+//     });
